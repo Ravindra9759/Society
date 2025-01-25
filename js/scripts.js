6c09c7b4199149427fe5c02a2edf5e63f76d57e8
Ravindra9759 <ravindra975902@gmail.com>
2025-01-25 10:41:29 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function adminLogin() {
+    alert("Admin Login Clicked");
+    // Add your admin login logic here
+}
+
+function userLogin() {
+    alert("User Login Clicked");
+    // Add your user login logic here
+}
+function UserLogin() {
+    alert("Login Clicked");
+    // Add your user login logic here
+}
